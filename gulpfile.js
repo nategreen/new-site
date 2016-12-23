73acedd9a3b68e8c781743e1d15c7e70221c72f9
Nate Green <nate.green@mrisoftware.com>
2016-12-22 23:12:38 -0500

    Set up remaining gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 // var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
-  var reload = browserSync.reload();
+  var reload = browserSync.reload;
 var clean = require('gulp-clean');
 var data = require('gulp-data');
 var environment = require('gulp-environments');
@@ -15,10 +15,10 @@ var hb = require('gulp-hb');
 var plumber = require('gulp-plumber');
 // var push = require('gulp-git-push');
 var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
 // var sassGlob = require('gulp-sass-glob');
 var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify');
 // var utils = require('gulp-util');
 
 var src = {
@@ -49,15 +49,12 @@ gulp.task('clean:html', function() {
 
 // Compile Sass files
 gulp.task('sass', function(){
-  gulp.src(src.sass)
+  return gulp.src(src.sass)
   .pipe(dev(sourcemaps.init()))
   .pipe(sass().on('error', sass.logError))
   .pipe(dev(sourcemaps.write()))
-  .pipe(prod(uglify()))
   .pipe(gulp.dest(dist.css));
 });
-
-gulp.task('sass:prod', ['env:prod', 'sass']);
 
 // Watch sass files for changes
 gulp.task('watch:sass', ['sass'], function(){
@@ -71,7 +68,7 @@ gulp.task('handlebars', ['clean:html'], function() {
     .helpers(hbHelpers)
     .partials(src.partials);
 
-  gulp.src(hbSource)
+  return gulp.src(hbSource)
     .pipe(plumber())
 
     // Pass data from front-matter
@@ -87,25 +84,36 @@ gulp.task('handlebars', ['clean:html'], function() {
     // Change the extensions from .html.hbs to .html
     .pipe(rename({
       extname: ''
-    }))    
+    }))
     .pipe(gulp.dest(dist.root));
   });
 
-// Task that watches Handlebars files for changes
+// Watch Handlebars files for changes
 gulp.task('watch:html', ['handlebars'], function(){
   gulp.watch([src.html, src.partials], ['handlebars']);
 });
 
-// Default task that compiles everything
-gulp.task('default', ['handlebars', 'sass'])
+// Compiles everything (development)
+gulp.task('default', ['handlebars', 'sass']);
 
 // Task that watches everything and runs a Browserlink server for development
 gulp.task('serve', ['default'], function(){
+  browserSync({
+    server: {
+      baseDir: dist.root
+    }
+  });
 
+  gulp.task('reload:html', ['handlebars'], reload);
+  gulp.task('reload:css', ['sass'], reload);
+
+  gulp.watch(src.sass, ['reload:css']);
+  gulp.watch([src.html, src.partials], ['reload:html']);
 });
 
-// Task that builds the site for development (once), with drafts
-
 // Task that builds the site for staging
+gulp.task('build', ['env:prod'], function(){
+  runSequence(['handlebars', 'sass'])
+});
 
 // Task that builds the site in production mode and pushes to GH pages
